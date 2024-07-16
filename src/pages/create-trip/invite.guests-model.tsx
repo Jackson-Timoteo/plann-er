53644f39d2afcdd934c08d88c8d45a6cbb6e64bc
Jackson-Timoteo <60791082+Jackson-Timoteo@users.noreply.github.com>
2024-07-16 18:01:54 -0300

    refactor: individually separating the components of the confirmTripmodal

--- src/pages/create-trip/invite.guests-model.tsx
+++ src/pages/create-trip/invite.guests-model.tsx
@@ -1,7 +1,7 @@
 import { AtSign, Plus, X } from "lucide-react";
 import { FormEvent } from "react";
 
-interface InviteGuestsMoodalProps {
+interface InviteGuestsModalProps {
     closeGuestsModal: () => void
     emailToInvite: string []
     addNewEmailToInvite: (event: FormEvent<HTMLFormElement>) => void
@@ -13,7 +13,7 @@ export function InviteGuestsModal({
     closeGuestsModal,
     emailToInvite,
     removeEmailFromInvites
-}: InviteGuestsMoodalProps) {
+}: InviteGuestsModalProps) {
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
