beb4c35531b1988f85b69441b85ddb7ca3e1ed9f
Jackson-Timoteo <60791082+Jackson-Timoteo@users.noreply.github.com>
2024-07-22 17:57:33 -0300

    refactor: separatingg the important links  modal component

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,6 +1,7 @@
 import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, UserCircle, X } from "lucide-react";
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
+import { ImpoortantLinks } from "./important-links";
 
 export function TripDetailsPage() {
   const [ isCreateActivityModalOpen, setIsCreateActivityModalOpen ] = useState(false);
@@ -82,38 +83,10 @@ export function TripDetailsPage() {
         </div>
 
         <div className="w-80 space-y-6">
-          <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Links Imporantes</h2>
-
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5 flex-1">
-                  <span className="block font-medium text-zinc-100">Reservas do AirBnB</span>
-                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                    https://www.youtube.com/watch?v=iejEg_JgGdsdsdadasdadasdasdasdasdsdaE0
-                  </a>
-                </div>
-                <Link2 className="text-zinc-400 size-5" />
-              </div>
-
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5 flex-1">
-                  <span className="block font-medium text-zinc-100">Regras da casa </span>
-                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                    https://www.youtube.com/watch?v=iejEg_JgGdsdsdadasdadasdasdasdasdsdaE0
-                  </a>
-                </div>
-                <Link2 className="text-zinc-400 size-5" />
-              </div>
-            </div>
-
-              <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
-                <Plus className="size-5" />
-                Cadastrar novo link
-              </button>
-          </div>
+          <ImpoortantLinks />
 
           <div className="w-full h-px bg-zinc-800" />
+          
           <div className="space-y-6">
             <h2 className="font-semibold text-xl">Convidados</h2>
 
