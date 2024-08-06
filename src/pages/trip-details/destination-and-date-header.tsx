ebb4d8f1d137a13a6084fa4bb91940b56ffe13a9
Jackson-Timoteo <jackson_siilva@hotmail.com>
2024-08-06 20:25:44 -0300

    feat: Creating routes to pick up all trip participants

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,28 +1,50 @@
 import { Calendar, MapPin, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
-export function DestinationAndDateHeader () {
-    return (
-        <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+interface Trip {
+  id: string 
+  destination: string
+  starts_at: string
+  ends_at: string
+  is_confirmed: string
+}
+
+export function DestinationAndDateHeader() {
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState<Trip | undefined>()
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}`).then(response => setTrip(response.data.trip))
+  }, [tripId]);
+  
+  const displayedDate = trip
+  ? format(trip.starts_at, "d' de 'LLL").concat(' até ').concat(format(trip.ends_at, "d' de 'LLL"))
+  : null;
+
+  return (
+    <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <MapPin className="siz-5 text-zinc-400" />
+        <span className="text-lg text-zinc-100">{trip?.destination}</span>
+      </div>
+
+      <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
-          <MapPin className="siz-5 text-zinc-400" />
-          <span className="text-lg text-zinc-100">Florianopolis, Brasil</span>
+          <Calendar className="siz-5 text-zinc-400" />
+          <span className="text-lg text-zinc-100">{displayedDate}</span>
         </div>
 
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Calendar className="siz-5 text-zinc-400" />
-            <span className="text-lg text-zinc-100">17 a 27 de agosto</span>
-          </div>
+        <div className="w-px h-6 bg-zinc-700" />
 
-          <div className="w-px h-6 bg-zinc-700" />
-
-          <Button variant="primary">
-            Alterar Local/Data
-            <Settings2 className="size-5" />
-          </Button>
-          
-        </div>
-      </div> 
-    )
+        <Button variant="primary">
+          Alterar Local/Data
+          <Settings2 className="size-5" />
+        </Button>
+      </div>
+    </div>
+  );
 }
--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,31 +1,48 @@
-import { CircleDashed, UserCircle } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCircle } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Participant {
+  id: string
+  name: string | null
+  email: string 
+  is_confirmed: boolean
+}
 
 export function Guests() {
+
+  const { tripId } = useParams();
+  const [participants, setParticipants] = useState<Participant[]>([])
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
+  }, [tripId]);
+  
+  
     return (
         <div className="space-y-6">
         <h2 className="font-semibold text-xl">Convidados</h2>
 
         <div className="space-y-5">
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5 flex-1">
-              <span className="block font-medium text-zinc-100">Walter White</span>
-              <span className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                ww@gmail.com
-              </span>
-            </div>
-            <CircleDashed className="text-zinc-400 size-5" />
-          </div>
-
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5 flex-1">
-              <span className="block font-medium text-zinc-100">Van Helsing </span>
-              <span className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                van@gmail.com
-              </span>
-            </div>
-            <CircleDashed className="text-zinc-400 size-5" />
-          </div>
+            {participants.map((participant, index) => {
+              return (
+                <div key={participant.id} className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5 flex-1">
+                  <span className="block font-medium text-zinc-100">{participant.name ?? `Convidado ${index}` } </span>
+                  <span className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                    {participant.email}
+                  </span>
+                </div>
+               {participant.is_confirmed ? (
+                <CheckCircle2 className="size-5 shrink-0 text-green-500" />
+               ): (
+                <CircleDashed className="text-zinc-400 size-5" />
+               )}
+              </div>
+              )
+            })}
         </div>
 
           <Button variant="primary" size="full">
@@ -35,4 +52,4 @@ export function Guests() {
           
       </div>
     )
-}
+} 
